eab6e3199fb74a5f064d5b4aaefbf4988da1d403
Gatien Anizan <gatien.anizan@gmail.com>
2019-02-20 13:19:49 +0100

    Username is now a unique key

--- src/requests/register.ts
+++ src/requests/register.ts
@@ -16,35 +16,46 @@ const register = (restifyReq: Request) => {
   return new Promise((resolve, reject) => {
     mongoose.connect(`mongodb://${config.mongo.user}:${config.mongo.pass}@${config.mongo.ip}:${config.mongo.port}/${config.mongo.collection}`, {useNewUrlParser: true})
     .then(() => {
-      let encyptedPassword: string;
+      // Check si le username est déjà pris
+      User.findOne({ username: restifyReq.body.username })
+      .then((res) => {
+        // Si il est libre 
+        console.log(res)
+        if (!res) {
+          let encyptedPassword: string;
 
-      // Encryption du password
-      hash(restifyReq.body.password, config.bcript.saltRound)
-      .then(hash => {
-        encyptedPassword = hash;
-        
-        // Création de l'objet user
-        let userInfo: UserInfo = {
-          username: restifyReq.body.username,
-          password: encyptedPassword,
-          email: restifyReq.body.email,
-          admin: false,
+          // Encryption du password
+          hash(restifyReq.body.password, config.bcript.saltRound)
+          .then(hash => {
+            encyptedPassword = hash;
+            
+            // Création de l'objet user
+            let userInfo: UserInfo = {
+              username: restifyReq.body.username,
+              password: encyptedPassword,
+              email: restifyReq.body.email,
+              admin: false,
+            }
+            
+            // Création du document User
+            let newUser = new User(userInfo);
+      
+            // Push du User dans la base
+            newUser.save()
+            .then(res => {
+              mongoose.disconnect();
+              // Then
+              resolve(res.toJSON());
+            }).catch(err => {
+              mongoose.disconnect();
+              // Catch
+              reject(err);
+            });
+          });
+        // Si il est déjà pris
+        } else {
+          reject('Username already taken');
         }
-        
-        // Création du document User
-        let newUser = new User(userInfo);
-  
-        // Push du User dans la base
-        newUser.save()
-        .then(res => {
-          mongoose.disconnect();
-          // Then
-          resolve(res.toJSON());
-        }).catch(err => {
-          mongoose.disconnect();
-          // Catch
-          reject(err);
-        });
       });
     });
   });
